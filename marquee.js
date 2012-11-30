978ed97689c831e061e05c191c7438d5d5ee1a84
Andy Brody <andy@stripe.com>
2012-11-29 23:05:08 -0800

    Add marquee! :eyes:

---
+++ marquee.js
@@ -0,0 +1,10 @@
+// Wrap <body> in a <marquee>!
+// NB: this will make document.body == null, so scripts that rely on it will be sad
+// This is less confusing than having two bodies or moving the body's children (which makes layout sad)
+function marqueeify() {
+    var marquee = document.createElement('marquee');
+    document.getElementsByTagName('html')[0].appendChild(marquee);
+    marquee.appendChild(html.getElementsByTagName('body')[0]);
+}
+
+// <a href="javascript:(function(){var marquee=document.createElement('marquee');document.getElementsByTagName('html')[0].appendChild(marquee);marquee.appendChild(html.getElementsByTagName('body')[0]);}())">Marqueeify!</a>
